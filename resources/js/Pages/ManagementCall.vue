571b57e6498f930b4544365fe4477453b3ea671f
jm-metalcash <jm@metalcash.be>
2024-11-22 11:13:50 +0100

    chore: add country and phone number in new client add and updated

--- resources/js/Pages/ManagementCall.vue
+++ resources/js/Pages/ManagementCall.vue
@@ -365,60 +365,74 @@ const formatDate = (date) => {
                                         >
                                             Nouveaux fournisseurs ajoutés
                                         </h3>
-                                        <div class="py-3 text-sm">
+                                        <div
+                                            v-for="(
+                                                user, index
+                                            ) in recentAddedUsers"
+                                            :key="user.id || index"
+                                            @click="selectUser(user)"
+                                            class="text-sm flex flex-wrap justify-between items-center cursor-pointer rounded-md px-2 py-2 my-2 w-full bg-orange-50 text-gray-700 hover:bg-orange-100 hover:text-orange-900"
+                                        >
+                                            <!-- Nom complet -->
                                             <div
-                                                v-for="(
-                                                    user, index
-                                                ) in recentAddedUsers"
-                                                :key="user.id || index"
-                                                @click="selectUser(user)"
-                                                class="flex flex-wrap justify-between items-center cursor-pointer rounded-md px-2 py-2 my-2 w-full bg-orange-50 text-gray-700 hover:bg-orange-100 hover:text-orange-900"
+                                                class="flex w-full sm:w-1/6 items-center"
                                             >
-                                                <!-- Nom complet -->
+                                                <span
+                                                    :class="[
+                                                        user.recently_added
+                                                            ? 'bg-fuchsia-500'
+                                                            : 'bg-green-400',
+                                                        'h-2 w-2 mr-4 rounded-full',
+                                                    ]"
+                                                ></span>
                                                 <div
-                                                    class="flex w-full sm:w-1/6 items-center"
+                                                    class="font-medium text-left"
                                                 >
-                                                    <span
-                                                        :class="[
-                                                            user.recently_added
-                                                                ? 'bg-fuchsia-500'
-                                                                : 'bg-green-400',
-                                                            'h-2 w-2 mr-4 rounded-full',
-                                                        ]"
-                                                    ></span>
-                                                    <div
-                                                        class="font-medium text-left"
-                                                    >
-                                                        {{ user.firstName }}
-                                                        {{ user.familyName }}
-                                                    </div>
+                                                    {{ user.firstName }}
+                                                    {{ user.familyName }}
                                                 </div>
-                                                <!-- Date d'ajout et utilisateur qui a ajouté -->
+                                            </div>
+
+                                            <!-- Numéro de téléphone -->
+                                            <div class="flex w-full sm:w-1/6">
                                                 <div
-                                                    class="text-sm w-full sm:w-1/6"
+                                                    class="font-medium text-left ml-6 md:ml-0"
                                                 >
-                                                    Ajouté par : <br />
-                                                    <span
-                                                        class="font-bold text-xs mr-2"
-                                                    >
-                                                        {{
-                                                            user.created_by
-                                                                ? user
-                                                                      .created_by
-                                                                      .name
-                                                                : "Inconnu"
-                                                        }}
-                                                    </span>
-                                                    <span
-                                                        class="font-bold text-xs"
-                                                    >
-                                                        {{
-                                                            formatDate(
-                                                                user.created_at
-                                                            )
-                                                        }}
-                                                    </span>
+                                                    {{ user.phone }}
                                                 </div>
+                                            </div>
+
+                                            <!-- Pays -->
+                                            <div class="flex w-full sm:w-1/6">
+                                                <div
+                                                    class="text-sm font-normal text-left ml-6 md:ml-0"
+                                                >
+                                                    {{ user.country }}
+                                                </div>
+                                            </div>
+
+                                            <!-- Date d'ajout et utilisateur ayant ajouté -->
+                                            <div
+                                                class="text-sm w-full sm:w-1/6"
+                                            >
+                                                Ajouté par : <br />
+                                                <span
+                                                    class="font-bold text-xs mr-2"
+                                                >
+                                                    {{
+                                                        user.created_by
+                                                            ? user.created_by
+                                                                  .name
+                                                            : "Inconnu"
+                                                    }}
+                                                </span>
+                                                <span class="font-bold text-xs">
+                                                    {{
+                                                        formatDate(
+                                                            user.created_at
+                                                        )
+                                                    }}
+                                                </span>
                                             </div>
                                         </div>
                                     </div>
@@ -433,56 +447,78 @@ const formatDate = (date) => {
                                         >
                                             Derniers fournisseurs modifiés
                                         </h3>
-                                        <div class="py-3 text-sm">
+                                        <div
+                                            v-for="(
+                                                user, index
+                                            ) in recentModifiedUsers"
+                                            :key="user.id || index"
+                                            @click="selectUser(user)"
+                                            class="text-sm flex flex-wrap justify-between items-center cursor-pointer rounded-md px-2 py-2 my-2 w-full bg-green-50 text-gray-700 hover:bg-green-100 hover:text-green-800"
+                                        >
+                                            <!-- Nom complet -->
                                             <div
-                                                v-for="(
-                                                    user, index
-                                                ) in recentModifiedUsers"
-                                                :key="user.id || index"
-                                                @click="selectUser(user)"
-                                                class="flex flex-wrap justify-between items-center cursor-pointer rounded-md px-2 py-2 my-2 w-full bg-green-50 text-gray-700 hover:bg-green-100 hover:text-green-800"
+                                                class="flex w-full sm:w-1/6 items-center"
                                             >
-                                                <!-- Nom complet -->
+                                                <span
+                                                    :class="[
+                                                        user.recently_added
+                                                            ? 'bg-fuchsia-500'
+                                                            : 'bg-green-400',
+                                                        'h-2 w-2 mr-4 rounded-full',
+                                                    ]"
+                                                ></span>
                                                 <div
-                                                    class="flex w-full sm:w-1/6 items-center"
+                                                    class="font-medium text-left"
                                                 >
-                                                    <span
-                                                        :class="[
-                                                            user.recently_added
-                                                                ? 'bg-fuchsia-500'
-                                                                : 'bg-green-400',
-                                                            'h-2 w-2 mr-4 rounded-full',
-                                                        ]"
-                                                    ></span>
-                                                    <div
-                                                        class="font-medium text-left"
-                                                    >
-                                                        {{ user.firstName }}
-                                                        {{ user.familyName }}
-                                                    </div>
+                                                    {{ user.firstName }}
+                                                    {{ user.familyName }}
                                                 </div>
-                                                <!-- Date de modification et utilisateur qui a modifié -->
+                                            </div>
+
+                                            <!-- Numéro de téléphone -->
+                                            <div class="flex w-full sm:w-1/6">
                                                 <div
-                                                    class="text-sm font-normal w-full sm:w-1/6"
+                                                    class="font-medium text-left ml-6 md:ml-0"
                                                 >
-                                                    Modifié par : <br />
-                                                    <span class="font-bold text-xs mr-2">
-                                                        {{
-                                                            user.updated_by
-                                                                ? user
-                                                                      .updated_by
-                                                                      .name
-                                                                : "Inconnu"
-                                                        }}
-                                                    </span>
-                                                    <span class="font-bold text-xs">
-                                                        {{
-                                                            formatDate(
-                                                                user.updated_at
-                                                            )
-                                                        }}
-                                                    </span>
+                                                    {{
+                                                        user.phone
+                                                    }}
                                                 </div>
+                                            </div>
+
+                                            <!-- Pays -->
+                                            <div class="flex w-full sm:w-1/6">
+                                                <div
+                                                    class="text-sm font-normal text-left ml-6 md:ml-0"
+                                                >
+                                                    {{
+                                                        user.country
+                                                    }}
+                                                </div>
+                                            </div>
+
+                                            <!-- Date de modification et utilisateur ayant modifié -->
+                                            <div
+                                                class="text-sm font-normal w-full sm:w-1/6"
+                                            >
+                                                Modifié par : <br />
+                                                <span
+                                                    class="font-bold text-xs mr-2"
+                                                >
+                                                    {{
+                                                        user.updated_by
+                                                            ? user.updated_by
+                                                                  .name
+                                                            : "Inconnu"
+                                                    }}
+                                                </span>
+                                                <span class="font-bold text-xs">
+                                                    {{
+                                                        formatDate(
+                                                            user.updated_at
+                                                        )
+                                                    }}
+                                                </span>
                                             </div>
                                         </div>
                                     </div>
